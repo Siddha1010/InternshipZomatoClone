9276bd7070df21e049e3323e4f4b1b9d08aa00f1
Siddha1010 <140188781+Siddha1010@users.noreply.github.com>
2023-07-21 21:29:17 +0530

    Add files via upload

---
+++ src/components/common/footer/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../footer/footer.css";
+
+const Footer = () => {
+  return <div className="absolute-center">Made with 🔥 by Siddhesh Gundal</div>;
+};
+
+export default Footer;
